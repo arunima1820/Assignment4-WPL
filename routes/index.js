fa1f4bef57548bc983b34f89b23ff88d726ffacd
Jesus19T <jesustapia4@gmail.com>
2022-11-20 20:00:37 -0600

    added post to reservations collection

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
+//var propertiesJSON = require("../properties.json")
 var router = express.Router();
 var monk = require('monk');
-var db = monk('localhost:27017/airbnb');
+var db = monk('localhost:27017/wpl-assign4');
 var collection = db.get('properties');
+var collection_resrv = db.get('reservations');
+
 
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -14,11 +17,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/properties', function(req, res) {
-  collection.find({}, function(err, properties){
-  if (err) throw err;
-   res.render('index', { properties : properties });
-  });
-  });
+	collection.find({}, function(err, properties){
+	if (err) throw err;
+	res.render('index', { properties : properties }); // instead it'd be properties:propertes when working from mongodb
+	});
+
+//   if (err) throw err;
+//   res.render('index', { properties : propertiesJSON }); // instead it'd be properties:propertes when working from mongodb
+
+});
+
 
 
   router.get('/properties/new', function(req, res) {
@@ -38,7 +46,21 @@ router.post('/properties', function(req, res) {
 		if (err) throw err;
 		// if insert is successfull, it will return newly inserted object
 	  	//res.json(video);
-		res.redirect('/videos');
+		res.redirect('/properties');
+	});
+});
+
+router.post('/reservations', function(req, res) {
+	collection_resrv.insert({ 
+		guest_id: req.body.guest_id,
+		check_in_date: req.body.check_in_date,
+		check_out_date: req.body.check_out_date,
+		amount_due:req.body.amount_due
+	}, function(err, video){
+		if (err) throw err;
+		// if insert is successfull, it will return newly inserted object
+	  	//res.json(video);
+		res.redirect('/properties');
 	});
 });
 
