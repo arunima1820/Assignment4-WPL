38c9ddf61edb29d7fd97b70188a56bd6bbc1aee6
Arunima Mittra <arunima1820@gmail.com>
2022-11-20 22:19:51 -0600

    fghgf h

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/properties', propertiesRouter);
+app.use('/properties', propertiesRouter);
 
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -7,78 +7,79 @@ var collection = db.get('properties');
 var collection_resrv = db.get('reservations');
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.redirect('/properties');
 });
 
-  router.get('/properties', function(req, res) {
-    collection.find({}, function(err, properties){
+router.get('/properties', function (req, res) {
+  collection.find({}, function (err, properties) {
     if (err) throw err;
-     res.render('index', { properties : properties });
-    });
-    });
-  
-
-    router.post('/properties/delete/:id', function(req, res) {
-      collection.remove({'_id': req.params.id }, function(err, property) {
-        if (err) throw err;
-        res.redirect('/properties')
-      })
-    })
-
-    router.get('/properties/update/:id', function(req, res) {
-      collection.findOne({ _id: req.params.id }, function(err, property){
-        if (err) throw err;
-        res.render('update', { property : property });
-        });
-    });
-
-    router.post('/properties/update/:id', function(req, res) {
-      collection.findOneAndUpdate({'_id': req.params.id }, 
-      {$set: 
-        { 
-          title: req.body.title,
-          description: req.body.desc,
-          pricePerNight: req.body.pricePerNight,
-          serviceFee: req.body.serviceFee,
-          cleaningFee: req.body.cleaningFee,
-          bedrooms: req.body.bedrooms,
-          beds: req.body.beds,
-          bathrooms: req.body.bathrooms,
-          location: {
-            city: req.body.city,
-            state: req.body.state,
-            country: req.body.country
-          },
-          pets: req.body.pets,
-          smoking: req.body.smoking,
-          propertyType: req.body.propertyType,
-          img: req.body.image,
-        }
-      }, 
-      function(err, result){
-        if (err) throw err;
-        res.redirect('/properties');
-      });
-    });
-
-
-  router.get('/properties/new', function(req, res) {
-    res.render('new');
-  
+    res.render('index', { properties: properties });
   });
+});
 
-  router.get('/properties/:id', function(req, res) {
-    collection.findOne({_id: req.params.id }, function(err, property){
+
+router.post('/properties/delete/:id', function (req, res) {
+  collection.remove({ '_id': req.params.id }, function (err, property) {
     if (err) throw err;
-    res.render('show', { property : property });
-    });
+    res.redirect('/properties')
+  })
+})
+
+router.get('/properties/update/:id', function (req, res) {
+  collection.findOne({ _id: req.params.id }, function (err, property) {
+    if (err) throw err;
+    res.render('update', { property: property });
   });
-  
-  
-router.post('/properties', function(req, res) {
-	collection.insert({ 
-		title: req.body.title,
+});
+
+router.post('/properties/update/:id', function (req, res) {
+  collection.findOneAndUpdate({ '_id': req.params.id },
+    {
+      $set:
+      {
+        title: req.body.title,
+        description: req.body.desc,
+        pricePerNight: req.body.pricePerNight,
+        serviceFee: req.body.serviceFee,
+        cleaningFee: req.body.cleaningFee,
+        bedrooms: req.body.bedrooms,
+        beds: req.body.beds,
+        bathrooms: req.body.bathrooms,
+        location: {
+          city: req.body.city,
+          state: req.body.state,
+          country: req.body.country
+        },
+        pets: req.body.pets,
+        smoking: req.body.smoking,
+        propertyType: req.body.propertyType,
+        img: req.body.image,
+      }
+    },
+    function (err, result) {
+      if (err) throw err;
+      res.redirect('/properties');
+    });
+});
+
+
+router.get('/properties/new', function (req, res) {
+  res.render('new');
+
+});
+
+router.get('/properties/:id', function (req, res) {
+  collection.findOne({ _id: req.params.id }, function (err, property) {
+    if (err) throw err;
+    res.render('show', { property: property });
+  });
+});
+
+
+router.post('/properties', function (req, res) {
+  collection.insert({
+    title: req.body.title,
     description: req.body.desc,
     pricePerNight: req.body.pricePerNight,
     serviceFee: req.body.serviceFee,
@@ -94,27 +95,42 @@ router.post('/properties', function(req, res) {
     pets: req.body.pets,
     smoking: req.body.smoking,
     propertyType: req.body.propertyType,
-		img: req.body.image,
-	}, function(err, property){
-		if (err) throw err;
-		// if insert is successfull, it will return newly inserted object
-	  	//res.json(video);
-		res.redirect('/properties');
-	});
+    img: req.body.image,
+  }, function (err, property) {
+    if (err) throw err;
+    // if insert is successfull, it will return newly inserted object
+    //res.json(video);
+    res.redirect('/properties');
+  });
 });
 
-router.post('/reservations', function(req, res) {
-	collection_resrv.insert({ 
-		guest_id: req.body.guest_id,
-		check_in_date: req.body.check_in_date,
-		check_out_date: req.body.check_out_date,
-		amount_due:req.body.amount_due
-	}, function(err, video){
-		if (err) throw err;
-		// if insert is successfull, it will return newly inserted object
-	  	//res.json(video);
-		res.redirect('/properties');
-	});
+router.get('/reservations/:id', function (req, res) {
+  collection.findOne({ _id: req.params.id }, function (err, reservation) {
+    if (err) throw err;
+    res.render('show_reservation', { reservation: reservation });
+  });
+});
+
+router.post('/reservations/:id', function (req, res) {
+  collection.remove({ _id: req.params.id }, function (err, reservation) {
+    if (err) throw err;
+    res.redirect('/properties');
+  });
+});
+
+
+router.post('/reservations', function (req, res) {
+  collection_resrv.insert({
+    guest_id: req.query.userId,
+    check_in_date: req.body.check_in_date,
+    check_out_date: req.body.check_out_date,
+    amount_due: req.body.amount_due
+  }, function (err, reservation) {
+    if (err) throw err;
+    // if insert is successfull, it will return newly inserted object
+    //res.json(video);
+    res.redirect('/properties');
+  });
 });
 
 
